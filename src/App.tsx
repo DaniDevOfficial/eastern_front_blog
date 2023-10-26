5e40191148e3cf57daaf70a73c619ae559ec8794
Léonard Roch <leonard.roch@lernende.bbw.ch>
2023-10-26 14:21:00 +0200

    added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { Heading } from "@chakra-ui/react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 export function App() {
   return (
-    <Heading h={"100%"} textAlign={"center"}>
-      Hello, World!
-    </Heading>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Heading h={"100%"} textAlign={"center"}>
+              Hello, world!
+            </Heading>
+          }
+        ></Route>
+        <Route path="*" element={<Navigate to="/" />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
