1ce70985febdc0a39f73b13272d4d74a657eb7d8
David <bischof.david.db@gmail.com>
2023-12-04 19:03:39 +0100

    Added basic landing page from figma

--- src/components/HomePage/LandingPage.tsx
+++ src/components/HomePage/LandingPage.tsx
@@ -1,4 +1,5 @@
-import { Box, Heading, Text, Link as ChakraLink, Icon } from "@chakra-ui/react";
+
+import { Box, Heading, Text, Link as ChakraLink, Icon, Center } from "@chakra-ui/react";
 import { Post } from "../../types/Post";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { Link as ReactRouterLink } from "react-router-dom";
@@ -9,69 +10,73 @@ interface LandingPageProps {
 
 export function LandingPage({ post: latestPost }: LandingPageProps) {
   return (
-    <Box
-      position="relative"
-      height="100vh"
-      backgroundImage={
-        latestPost ? `url(${latestPost.image?.src})` : "url(placeholder.jpg)"
-      }
-      backgroundSize="cover"
-      backgroundPosition="center"
-    >
-      <Box
-        position="absolute"
-        top="0"
-        left="0"
-        width="100%"
-        height="100%"
-        background="linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))"
-      />
-      <Box
-        position="absolute"
-        top="50%"
-        left="50%"
-        transform="translate(-50%, -50%)"
-        textAlign="center"
-        color="white"
-      >
-        <Heading>Ukrain Russia War</Heading>
-      </Box>
+    <Center>
 
       <Box
-        position="absolute"
-        bottom="0"
-        right="0"
-        textAlign={{ base: "center", md: "left" }}
-        paddingRight={{ base: "10vw", md: "70%" }}
-        paddingLeft={{ base: "10vw", md: "3vw" }}
-        color="white"
-        paddingBottom={{ base: "2vh", md: "2vh" }}
+        position="relative"
+        marginTop="10vh"
+        height="80vh"
+        backgroundImage={
+          latestPost ? `url(${latestPost.image?.src})` : "url(placeholder.jpg)"
+        }
+        backgroundSize="cover"
+        backgroundPosition="center"
+        width="80%"
+        borderRadius="10px"
+
       >
-        <Heading
-          fontSize={{ base: "1.3rem", md: "1.5rem" }}
-          paddingBottom={{ base: "1vh", md: "1vh" }}
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          width="100%"
+          height="100%"
+          background="linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))"
+          borderRadius="10px"
+
+        />
+        <Box
+          position="absolute"
+          bottom="0"
+          right="0"
+          textAlign={{ base: "center", md: "left" }}
+          paddingRight={{ base: "10vw", md: "70%" }}
+          paddingLeft={{ base: "10vw", md: "3vw" }}
+          color="white"
+          paddingBottom={{ base: "5%", md: "5%" }}
         >
-          {latestPost ? latestPost.title : "Lorem ipsum dolor sit amet"}
-        </Heading>
-        <Text fontSize={{ base: "0.9rem", md: "1rem" }}>
-          {latestPost
-            ? latestPost.subtitle
-            : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima tempore ratione quas pariatur modi magnam voluptate dolorem, quo facere repudiandae incidunt earum autem dolore iste cum nam aliquid beatae. Velit."}
-        </Text>
-        {latestPost && (
-          <ChakraLink
-            as={ReactRouterLink}
-            to={`/post/${latestPost.id}`}
-            display="flex"
-            alignItems="center"
-            color="teal.500"
-            _hover={{ color: "teal.700" }}
+          <Heading
+            fontSize={{ base: "1.3rem", md: "1.5rem" }}
+            paddingBottom={{ base: "1vh", md: "1vh" }}
           >
-            Mehr lesen
-            <Icon as={AiOutlineArrowRight} boxSize={4} ml={1} />
-          </ChakraLink>
-        )}
+            {latestPost ? latestPost.title : "Lorem ipsum dolor sit amet"}
+          </Heading>
+
+          {latestPost && (
+            <Box
+          
+              p={2}
+              position="absolute"
+              alignItems="center"
+              color="#FEC709"
+              border={{ base: "1px solid #FEC709" }}
+              borderRadius="10px"
+              
+            >
+              <ChakraLink
+                as={ReactRouterLink}
+                to={`/post/${latestPost.id}`}
+                display="flex"
+                alignItems="center"
+                color="#FEC709"
+              >
+                Mehr lesen
+                <Icon as={AiOutlineArrowRight} boxSize={4} ml={1} />
+              </ChakraLink>
+            </Box>
+          )}
+        </Box>
       </Box>
-    </Box>
+    </Center>
   );
 }
