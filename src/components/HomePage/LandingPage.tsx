e9f2c75683686e80c87b8b5e003b7ebdd62cb9ef
David <bischof.david.db@gmail.com>
2023-12-04 19:38:49 +0100

    more better

--- src/components/HomePage/LandingPage.tsx
+++ src/components/HomePage/LandingPage.tsx
@@ -1,7 +1,7 @@
 
 import { Box, Heading, Text, Link as ChakraLink, Icon, Center } from "@chakra-ui/react";
 import { Post } from "../../types/Post";
-import { AiOutlineArrowRight } from "react-icons/ai";
+import { AiOutlineArrowDown, AiOutlineArrowRight } from "react-icons/ai";
 import { Link as ReactRouterLink } from "react-router-dom";
 
 interface LandingPageProps {
@@ -10,73 +10,96 @@ interface LandingPageProps {
 
 export function LandingPage({ post: latestPost }: LandingPageProps) {
   return (
-    <Center>
+    <Box>
+      <Center>
 
-      <Box
-        position="relative"
-        marginTop="10vh"
-        height="80vh"
-        backgroundImage={
-          latestPost ? `url(${latestPost.image?.src})` : "url(placeholder.jpg)"
-        }
-        backgroundSize="cover"
-        backgroundPosition="center"
-        width="80%"
-        borderRadius="10px"
-
-      >
         <Box
-          position="absolute"
-          top="0"
-          left="0"
-          width="100%"
-          height="100%"
-          background="linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))"
+          position="relative"
+          marginTop="10vh"
+          height="80vh"
+          backgroundImage={
+            latestPost ? `url(${latestPost.image?.src})` : "url(placeholder.jpg)"
+          }
+          backgroundSize="cover"
+          backgroundPosition="center"
+          width="80%"
           borderRadius="10px"
 
-        />
-        <Box
-          position="absolute"
-          bottom="0"
-          right="0"
-          textAlign={{ base: "center", md: "left" }}
-          paddingRight={{ base: "10vw", md: "70%" }}
-          paddingLeft={{ base: "10vw", md: "3vw" }}
-          color="white"
-          paddingBottom={{ base: "5%", md: "5%" }}
         >
-          <Heading
-            fontSize={{ base: "1.3rem", md: "1.5rem" }}
-            paddingBottom={{ base: "1vh", md: "1vh" }}
-          >
-            {latestPost ? latestPost.title : "Lorem ipsum dolor sit amet"}
-          </Heading>
+          <Box
+            position="absolute"
+            top="0"
+            left="0"
+            width="100%"
+            height="100%"
+            background="rgba(0, 0, 0, 0.4)"
+            borderRadius="10px"
 
-          {latestPost && (
-            <Box
-          
-              p={2}
-              position="absolute"
-              alignItems="center"
-              color="#FEC709"
-              border={{ base: "1px solid #FEC709" }}
-              borderRadius="10px"
-              
+          />
+          <Box
+            position="absolute"
+            bottom="0"
+            right="0"
+            textAlign={{ base: "center", md: "left" }}
+            paddingRight={{ base: "10vw", md: "70%" }}
+            paddingLeft={{ base: "10vw", md: "3vw" }}
+
+            color="white"
+            paddingBottom={{ base: "5%", md: "10%" }}
+          >
+            <Heading
+              fontSize={{ base: "1.3rem", md: "1.5rem" }}
+              paddingBottom={{ base: "1vh", md: "1vh" }}
             >
-              <ChakraLink
-                as={ReactRouterLink}
-                to={`/post/${latestPost.id}`}
-                display="flex"
+              {latestPost ? latestPost.title : "Lorem ipsum dolor sit amet"}
+            </Heading>
+
+            {latestPost && (
+              <Box
+                _hover={{ cursor: "pointer" }}
+                p={2}
+                position="absolute"
                 alignItems="center"
                 color="#FEC709"
+                border={{ base: "1px solid #FEC709" }}
+                borderRadius="10px"
+
               >
-                Mehr lesen
-                <Icon as={AiOutlineArrowRight} boxSize={4} ml={1} />
-              </ChakraLink>
-            </Box>
-          )}
+                <ChakraLink
+                  as={ReactRouterLink}
+                  to={`/post/${latestPost.id}`}
+                  display="flex"
+                  alignItems="center"
+                  color="#FEC709"
+                >
+                  Mehr lesen
+                  <Icon as={AiOutlineArrowRight} boxSize={4} ml={1} />
+                </ChakraLink>
+              </Box>
+            )}
+          </Box>
         </Box>
+
+      </Center>
+  
+      <Box
+        position="relative"
+        left="50%"
+        marginTop="5vh"
+        color="#FEC709"
+
+
+      >
+
+
+
+        <Icon
+          onClick={() => {
+            window.scrollTo({ top: window.innerHeight, behavior: "smooth" });
+          }}
+
+        as={AiOutlineArrowDown} boxSize={6} />
       </Box>
-    </Center>
+    </Box>
   );
 }
