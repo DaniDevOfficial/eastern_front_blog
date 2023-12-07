ca1309a482b8ad963df468f7aac8d26bdc71805a
David <bischof.david.db@gmail.com>
2023-12-07 19:31:58 +0100

    added basic double poost dispaly

--- src/components/HomePage/DesktopHomeMain.tsx
+++ src/components/HomePage/DesktopHomeMain.tsx
@@ -5,6 +5,8 @@ import {
   Image,
   Link as ChakraLink,
   Icon,
+  Flex,
+  Toast,
 } from "@chakra-ui/react";
 import { Link as ReactRouterLink, useLocation } from "react-router-dom";
 import { AiOutlineArrowRight } from "react-icons/ai";
@@ -23,6 +25,8 @@ export function DesktopHomeMain({ posts }: MobileHomeMainProps) {
   const handleCopyLink = (postId: string) => {
     const linkToCopy = `${currentUrl}post/${postId}`;
     navigator.clipboard.writeText(linkToCopy);
+    alert("Link copied to clipboard");
+
   };
 
   const Article = ({ post, index }: { post: { title: string; content: string }; index: number }) => {
@@ -34,22 +38,119 @@ export function DesktopHomeMain({ posts }: MobileHomeMainProps) {
 
     if (articleStyleClass === 'double') {
       return (
-        <div>
-          double
-        </div>
+        <Box
+          width="37.5vw"
+          backgroundColor="#1C1C20"
+          borderRadius="20px"
+
+        >
+          <Box
+            height="40vh"
+            width="100%"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Image
+              padding={2}
+              borderRadius="20px"
+              src={post.image?.src}
+              alt={post.image.source}
+              height="100%"
+              width="100%"
+              objectFit="cover"
+            />
+          </Box>
+          <Box
+            p={4}
+          >
+            <Heading
+              as="h2"
+              size="md"
+              mb={2}
+            >
+              {post.title}
+            </Heading>
+            <Text
+              fontSize="sm"
+              color="#FFFFFF"
+              mb={2}
+            >
+              {post.subtitle}
+            </Text>
+            <Box
+              alignItems="center"
+              justifyContent="space-between"
+
+            >
+              <Box
+                _hover={{ cursor: "pointer" }}
+                p={2}
+                display="inline-flex"
+                color="#FEC709"
+                border={{ base: "1px solid #FEC709" }}
+                borderRadius="10px"
+                mb={2}
+              >
+                <ChakraLink
+                  as={ReactRouterLink}
+                  to={`/post/${post.id}`}
+                  color="#FEC709"
+                >
+                  Mehr lesen
+                  <Icon as={AiOutlineArrowRight} boxSize={4} ml={1} />
+                </ChakraLink>
+              </Box>
+              <Flex
+                alignItems="center"
+                gap={2}
+              >
+                <Text
+                  fontSize="sm"
+                  color="gray.400"
+                >
+                  Author
+                </Text>
+                <Text
+                  fontSize="sm"
+                  color="gray.400"
+                >
+                  Date
+                </Text>
+                <Text
+                  fontSize="sm"
+                  color="gray.400"
+                >
+                  Time To read
+                </Text>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  gap={2}
+                >
+                  <Icon
+                    as={CiShare2}
+                    onClick={() => handleCopyLink(post.id)}
+                    cursor="pointer"
+                  />
+                </Box>
+                
+              </Flex>
+            </Box>
+          </Box>
+        </Box>
       )
     } else if (articleStyleClass === 'doublev2') {
       return (
-        <div>
+        <Box width="80vw">
           doublev2
-        </div>
+        </Box>
       )
 
     } else if (articleStyleClass === 'single') {
       return (
-        <div>
-          single
-         </div>
+        <Box width="80vw">
+          Single
+        </Box>
       )
 
     }
@@ -65,10 +166,15 @@ export function DesktopHomeMain({ posts }: MobileHomeMainProps) {
   };
 
   return (
-    <Box>
+    <Flex
+      flexWrap="wrap"
+      marginLeft="10vw"
+      mt="10vh"
+      gap="5vw"
+    >
       {multipliedPosts.map((post, index) => (
         <Article key={index} post={post} index={index} />
       ))}
-    </Box>
+    </Flex>
   );
 }
