adf23c8754f399d8af622d6eaacfbb2005fe4071
Léonard Roch <leonard.roch@lernende.bbw.ch>
2023-12-21 17:19:15 +0100

    readded tts after removing it🙄

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,19 +1,33 @@
-import { LoaderFunctionArgs, useLoaderData } from "react-router-dom";
-import { getArticleById, getPostById } from "../repo/repo";
+import {
+  LoaderFunctionArgs,
+  useLoaderData,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import { getAllPosts, getArticleById, getPostById } from "../repo/repo";
 import { Post } from "../types/Post";
 import { Article } from "../types/Article";
 import {
+  Box,
   Container,
   Divider,
   Flex,
+  HStack,
   Heading,
+  Icon,
   Image,
-  Spacer,
+  Link,
   Text,
   chakra,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import ReactMarkdown from "react-markdown";
 import ChakraUIRenderer from "chakra-ui-markdown-renderer";
+import { CiBookmark, CiClock1, CiVolume, CiVolumeHigh } from "react-icons/ci";
+import { useEffect, useRef, useState } from "react";
+import { SingleArticleContainer } from "../components/HomePage/SingleArticleContainer";
+import { Utils } from "../dateUtils";
+import { ArrowBackIcon } from "@chakra-ui/icons";
 
 interface LoaderData {
   post: Post;
@@ -32,7 +46,6 @@ export async function loadPost({ params }: LoaderFunctionArgs) {
       status: 500,
     });
   }
-
   try {
     article = await getArticleById(post.article_id);
   } catch (error: any) {
@@ -46,43 +59,208 @@ export async function loadPost({ params }: LoaderFunctionArgs) {
 
 export function ArticlePage() {
   const { post, article } = useLoaderData() as LoaderData;
+  const [olderPosts, setOlderPosts] = useState<Post[]>([]);
+  const [readingTime, setReadingTime] = useState<number>(0);
+  const navigate = useNavigate();
+
+  enum ArticleStyle {
+    Double = "double",
+    Single = "single",
+    SmallImageLeft = "smallImageLeft",
+    SmallImageRight = "smallImageRight",
+  }
+
+  const Article = ({ post, index }: { post: Post; index: number }) => {
+    let articleStyleClass: ArticleStyle = ArticleStyle.Double;
+    let pattern = [];
+
+    pattern = [ArticleStyle.Single];
+
+    articleStyleClass = pattern[index % pattern.length];
+
+    return (
+      <SingleArticleContainer
+        post={post}
+        type={articleStyleClass as "smallImageLeft" | "smallImageRight"}
+      />
+    );
+  };
+  const location = useLocation();
+
+  useEffect(() => {
+    getAllPosts()
+      .then((posts) => {
+        const sortedPosts = posts;
+        if (sortedPosts.length === 0) throw new Error("No posts found");
+        const currentArticleId = post.id;
+        const latestPostId = sortedPosts[0]?.id;
+        const olderPosts = sortedPosts.filter(
+          (post) => post.id !== currentArticleId && post.id !== latestPostId
+        );
+        const topTwoOlderPosts = olderPosts.slice(0, 2);
+        setOlderPosts(topTwoOlderPosts);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    let text = textRef.current?.innerText;
+    let wordCount: any = text?.split(/\s/).filter(function (n) {
+      return n !== "";
+    }).length;
+    setReadingTime(Math.ceil(wordCount / 200));
+    // scroll to top
+    window.scrollTo(0, 0);
+  }, [location]);
+  function handleBookmark(id: string): void {
+    throw new Error("Function not implemented.");
+  }
+
+  const textRef = useRef<HTMLDivElement | null>(null);
+  function TextToSpeach(): void {
+    const utterance = new SpeechSynthesisUtterance(
+      textRef.current?.innerText ?? ""
+    );
+
+    utterance.lang = "de-DE";
+    utterance.rate = 1;
+    utterance.pitch = 1;
+    utterance.volume = 1;
+
+    speechSynthesis.speak(utterance);
+  }
   return (
     <>
-      <Container>
-        <Heading textAlign={"center"}>{post.title}</Heading>
-        <Text textAlign={"center"}>{post.subtitle}</Text>
-        <Divider />
-        <Flex justify={"center"}>
-          {post.published_at && (
-            <Text>Hochgeladen am {post.published_at?.toDateString()}</Text>
-          )}
+      <Container maxW="container.lg">
+        <HStack
+          color={"white"}
+          align={"center"}
+          cursor={"pointer"}
+          onClick={() => navigate(-1)}
+        >
+          <Icon aria-label="Zurück" as={ArrowBackIcon} />
+          <Text variant={"ghost"}>Zurück</Text>
+        </HStack>
+        <Flex my={10}>
+          <Heading as="h1" size="2xl" mb={4}>
+            {post.title}
+          </Heading>
+          <Icon
+            _hover={{ cursor: "pointer" }}
+            onClick={() => handleBookmark(post.id)}
+            as={CiBookmark}
+            ml={2}
+            boxSize={5}
+          />
         </Flex>
-        {post.image && (
-          <Flex justify={"center"} direction={"column"}>
-            <Image
-              src={post.image.src}
-              alt={post.image.description ?? "image..."}
-            />
-            <Flex justify={"space-between"}>
-              <Text>{post.image.description ?? ""}</Text>
-              <Spacer />
-              <Text>{post.image.source}</Text>
+        <Text color="#FFFFFF" mb={2}>
+          {post.subtitle}
+        </Text>
+        <Divider />
+        <Flex justifyContent="space-between" my={4}>
+          <HStack gap={3}>
+            <Text mb={2} color="grey">
+              {post.author}
+            </Text>
+            <Text color="grey">
+              {!!post.published_at &&
+                Utils.formatMessageDate(post.published_at)}
+            </Text>
+          </HStack>
+          <Text mb={2} color="grey"></Text>
+          <HStack gap={"18px"}>
+            <Flex color={"gray"} align={"center"}>
+              <Text color="grey" _hover={{ cursor: "pointer" }} marginRight={2}>
+                {readingTime} min
+              </Text>
+              <Icon _hover={{ cursor: "pointer" }} as={CiClock1} boxSize={5} />
             </Flex>
-          </Flex>
-        )}
+
+            <Flex color={"gray"} align={"center"}>
+              <Text
+                color={"grey"}
+                onClick={() => TextToSpeach()}
+                _hover={{ cursor: "pointer" }}
+                marginRight={2}
+              >
+                Vorlesen
+              </Text>
+              <Icon as={CiVolumeHigh} boxSize={5} />
+            </Flex>
+          </HStack>
+        </Flex>
+        <Image
+          src={post.image?.src}
+          alt={post.image?.description}
+          mb={4}
+          borderRadius={10}
+          width="100%"
+          maxH="70vh"
+          fit={"cover"}
+        />
+        <Flex justifyContent="space-between">
+          <Text mb={2} color="grey" fontStyle={"italic"}>
+            {post.image?.description}
+          </Text>
+          <Link href={post.image?.source} target="_blank" mb={2} color="grey">
+            {post.image?.source}
+          </Link>
+        </Flex>
+        <Divider />
+        <chakra.div mt={8} mb={4} color="#FFFFFF">
+          <Container ref={textRef} maxW="80vw">
+            {article.text.split("\n").map((paragraph, index) => (
+              <chakra.div key={index}>
+                <ReactMarkdown
+                  skipHtml
+                  components={ChakraUIRenderer()}
+                  key={index}
+                >
+                  {paragraph}
+                </ReactMarkdown>
+              </chakra.div>
+            ))}
+          </Container>
+        </chakra.div>
+        <Box
+          className="hr-lines"
+          maxW="500px"
+          mx="auto"
+          my="40px"
+          textAlign="center"
+          position="relative"
+        >
+          <Box
+            as="span"
+            pos="absolute"
+            top="50%"
+            left="0"
+            transform="translateY(-50%)"
+            h="2px"
+            w={{ md: "130px", base: "100px" }}
+            bg="#FEC709"
+            display="block"
+            content=""
+          />
+          <Box
+            as="span"
+            pos="absolute"
+            top="50%"
+            right="0"
+            transform="translateY(-50%)"
+            h="2px"
+            w={{ md: "130px", base: "100px" }}
+            bg="#FEC709"
+            display="block"
+            content=""
+          />
+          <Text>Ältere Artikel</Text>
+        </Box>
       </Container>
-      <Container>
-        {article.text
-          .replaceAll("\\n", "\n")
-          .split("\n")
-          .map((paragraph, index) => (
-            <chakra.div key={index}>
-              <ReactMarkdown components={ChakraUIRenderer()} key={index}>
-                {paragraph}
-              </ReactMarkdown>
-            </chakra.div>
-          ))}
-      </Container>
+      <Box marginLeft="10vw">
+        {olderPosts.map((post, index) => (
+          <Article key={index} post={post} index={index} />
+        ))}
+      </Box>
     </>
   );
 }
