0430f3564610b2028a845d4b533ea31374e44bbe
Léonard Roch <leonard.roch@lernende.bbw.ch>
2023-12-22 20:51:35 +0100

    corrected footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 
 const pages = [
   { name: "Timeline", path: "/timeline" },
-  { name: "Impressum", path: "/impressum" },
   { name: "Datenschutz", path: "/datenschutz" },
 ];
 
